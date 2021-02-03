bb253601c5665b1b2247eeefff924e7843125c9b
Beto Harris <harrisduque@gmail.com>
2021-02-03 07:15:46 -0500

    email field for Customer

--- bookingapp/models.py
+++ bookingapp/models.py
@@ -21,6 +21,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
     birthday = models.DateField()
+    email = models.EmailField()
 
     def __str__(self):
         return self.name

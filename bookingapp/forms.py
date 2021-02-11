141c6b4ae50aa523cd2c29d46235cc649893870a
Beto Harris <harrisduque@gmail.com>
2021-02-11 01:01:22 -0500

    Booking Form fields

--- bookingapp/forms.py
+++ bookingapp/forms.py
@@ -1,8 +1,12 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 from .models import Booking
 
 
 class BookingForm(ModelForm):
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = ['day', 'time_slot', 'asset', 'customer']

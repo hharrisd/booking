5d35c2a0ab02d14b29ff0e03295461954580a892
Beto Harris <harrisduque@gmail.com>
2021-02-11 01:00:54 -0500

    Time Slot and Booking tests

--- bookingapp/tests.py
+++ bookingapp/tests.py
@@ -1,11 +1,30 @@
-from datetime import datetime
+from contextlib import contextmanager
+from datetime import datetime, timedelta
 
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
 from django.test import TestCase
-from .models import TimeSlot
+from django.utils import timezone
+
+from .models import TimeSlot, Booking, Customer, Category, Asset
+from .forms import BookingForm
 
 
-class TimeSlotModelTests(TestCase):
+class ValidationErrorTestMixin(object):
+
+    @contextmanager
+    def assertValidationErrors(self, fields):
+        """
+        Assert that a validation error is raised, containing all the specified
+        fields, and only the specified fields.
+        """
+        try:
+            yield
+            raise AssertionError("ValidationError not raised")
+        except ValidationError as e:
+            self.assertEqual(set(fields), set(e.message_dict.keys()))
+
+
+class TimeSlotModelTests(TestCase, ValidationErrorTestMixin):
     @classmethod
     def setUpTestData(cls):
         checkin = datetime.strptime('06:00', '%H:%M').time()
@@ -17,7 +36,7 @@ class TimeSlotModelTests(TestCase):
         Checks if time slot has the proper time format: '%H:%M
         """
         timeslot = TimeSlot.objects.get(id=1)
-        self.assertEquals(str(timeslot), 'Slot A: 06:00 to 07:00')
+        self.assertEquals(str(timeslot), 'Slot A: 06:00 to 06:30')
 
     def test_time_diference(self):
         """
@@ -27,12 +46,79 @@ class TimeSlotModelTests(TestCase):
         # diff = timeslot.check_out.hour - timeslot.check_in.hour
         checkin = datetime.strptime('09:00', '%H:%M').time()
         checkout = datetime.strptime('09:00', '%H:%M').time()
-        self.assertRaises(ValidationError, TimeSlot, slot='A', check_in=checkin, check_out=checkout)
+        time_slot = TimeSlot(slot='T', check_in=checkin, check_out=checkout)
+        with self.assertValidationErrors([NON_FIELD_ERRORS]):
+            time_slot.full_clean()
 
     def test_range_hour(self):
         """
-        Checks if checking is not earlier than 7 AM
+        Checks if check in is not earlier than 7 AM
+        and check out is not later than 19:00
         """
         checkin = datetime.strptime('06:00', '%H:%M').time()
-        checkout = datetime.strptime('08:30', '%H:%M').time()
-        self.assertRaises(ValidationError, TimeSlot, slot='A', check_in=checkin, check_out=checkout)
+        checkout = datetime.strptime('20:30', '%H:%M').time()
+        time_slot = TimeSlot(slot='S', check_in=checkin, check_out=checkout)
+
+        with self.assertValidationErrors(['__all__']):
+            time_slot.full_clean()
+
+
+def create_customer():
+    """
+    Creates a booking object
+    :return: Customer object
+    """
+    customer_name = 'Test Customer'
+    customer_address = 'Test Street 125'
+    customer_birthday = datetime(1980, 12, 31)
+    customer_email = 'test@test.com'
+    return Customer.objects.create(name=customer_name, address=customer_address, birthday=customer_birthday,
+                                   email=customer_email)
+
+
+def create_slot():
+    check_in = datetime.strptime('09:00', '%H:%M').time()
+    check_out = datetime.strptime('10:00', '%H:%M').time()
+    return TimeSlot.objects.create(slot='T', check_in=check_in, check_out=check_out)
+
+
+# def create_booking(days):
+#     """
+#     Creates a booking object
+#     :param days: creation date difference in days
+#     :return: Booking object
+#     """
+#     day = timezone.now() + timedelta(days=days)
+#
+#
+#     test_customer = create_customer()
+#     test_category = Category.objects.create(category='Testing')
+#     test_asset = Asset.objects.create(category=test_category, description='Test Asset', comment='An asset to test')
+#     test_time_slot = create_slot()
+#     return Booking.objects.create(day=day, time_slot=test_time_slot, asset=test_asset, customer=test_customer)
+
+
+class BookingFormTests(TestCase):
+    def test_booking_in_past_time(self):
+        """
+        Checks that a booking is not made in past time
+        """
+
+        day = timezone.now() + timedelta(days=-10)
+
+        test_customer = create_customer()
+        test_category = Category.objects.create(category='Testing')
+        test_asset = Asset.objects.create(category=test_category, description='Test Asset', comment='An asset to test')
+        test_time_slot = create_slot()
+        # return Booking.objects.create(day=day, time_slot=test_time_slot, asset=test_asset, customer=test_customer)
+        booking_form = BookingForm(
+            data={
+                'day': day,
+                'time_slot': test_time_slot,
+                'asset': test_asset,
+                'customer': test_customer
+            })
+
+        self.assertEqual(
+            booking_form.errors["day"], ["A booking cannot be made in the past"]
+        )

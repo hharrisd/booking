274784479b06e7b61f94469e0135728cda15c887
Beto Harris <harrisduque@gmail.com>
2021-02-11 01:03:50 -0500

    Models validations

--- bookingapp/models.py
+++ bookingapp/models.py
@@ -1,11 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 import datetime
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.category
@@ -13,8 +14,8 @@ class Category(models.Model):
 
 class Asset(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    description = models.CharField(max_length=100)
-    comment = models.CharField(max_length=200)
+    description = models.CharField(max_length=100, unique=True)
+    comment = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.description
@@ -31,14 +32,31 @@ class Customer(models.Model):
 
 
 class TimeSlot(models.Model):
-    slot = models.CharField(max_length=1)
+    slot = models.CharField(max_length=1, unique=True)
     check_in = models.TimeField()
     check_out = models.TimeField()
 
     def clean(self):
+        cleaned_data = super().clean()
+        validation_error_list = []
+
         """ Validate that time slot has at least 1 hour of duration """
         if self.check_out.hour - self.check_in.hour < 1:
-            raise ValidationError(_('A time slot must have at least 1 hour of duration'))
+            validation_error_list.append(
+                ValidationError(_('A time slot must have at least 1 hour of duration'), code='invalid'))
+        """ Checks if check in is not earlier than 7 AM """
+        if self.check_in.hour < timezone.localtime().strptime('07:00', '%H:%M').hour:
+            validation_error_list.append(
+                ValidationError({'check_in': _('Check in cannot be earlier than 07:00')}, code='invalid'))
+        """ Checks if check out is not later than 19:00 """
+        if self.check_out.hour > timezone.localtime().strptime('19:00', '%H:%M').hour:
+            validation_error_list.append(
+                ValidationError({'check_in': _('Check out cannot be later than 19:00')}, code='invalid'))
+
+        if validation_error_list:
+            raise ValidationError(validation_error_list)
+
+        return cleaned_data
 
     def __str__(self):
         return f'Slot {self.slot}: {self.check_in.strftime("%H:%M")} to {self.check_out.strftime("%H:%M")}'
@@ -50,7 +68,14 @@ class Booking(models.Model):
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
+    def clean(self):
+        now = timezone.now()
+
+        if self.day < now.date():
+            raise ValidationError({'day': _('A booking cannot be made in the past')})
+
     def __str__(self):
         return f'{self.asset.description}, booked to {self.customer}, ' \
+               f'on {self.day.strftime("%x")}, ' \
                f'from {self.time_slot.check_in.strftime("%H:%M")} ' \
                f'to {self.time_slot.check_out.strftime("%H:%M")}'
